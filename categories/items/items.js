7ccf5318171b3a6c9c99e619d8ca427783e6655e
Cristian <lcristianiim@yahoo.com>
2017-02-22 15:26:40 +0200

    Added configuration and controller

--- categories/items/items.js
+++ categories/items/items.js
@@ -1,6 +1,25 @@
-angular.module("items", [
+angular.module("categories.items", [
+    "ui.router",
     "categories.items.create",
     "categories.items.edit",
     "ticketer.models.categories",
     "ticketer.models.items"
-]);
+])
+    .config(function($stateProvider){
+        $stateProvider
+            .state("ticketer.categories.items", {
+                url: "/categories/:category",
+                views: {
+                    "items@": {
+                        templateUrl: "categories/items/items.tmpl.html",
+                        controller: "ItemsController"
+                    }
+                }
+            })
+    })
+
+    .controller("ItemsController", function($scope, $stateParams) {
+        console.log("The state Parames", $stateParams);
+        $scope.currentCategoryName = $stateParams.category;
+    })
+;
