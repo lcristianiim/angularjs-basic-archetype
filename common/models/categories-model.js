b275d4f43b171da1976d311d98bd038244cce732
Cristian <lcristianiim@yahoo.com>
2017-02-23 11:18:07 +0200

    Control prommises with

--- common/models/categories-model.js
+++ common/models/categories-model.js
@@ -1,12 +1,14 @@
 angular.module("ticketer.models.categories", [
 
 ])
-    .service("CategoriesModel", function ($http) {
+    .service("CategoriesModel", function ($http, $q) {
        var model = this,
 
            URLS = {
                FETCH: "data/categories.json"
-           };
+           },
+
+           categories;
 
         function extract(result) {
             return result.data;
@@ -18,7 +20,27 @@ angular.module("ticketer.models.categories", [
         }
 
         model.getCategories = function () {
-            return $http.get(URLS.FETCH).then(cacheCategories)
+            return (categories) ? q.when(categories) : $http.get(URLS.FETCH).then(cacheCategories);
+        };
+
+        model.getCategoryByName = function (categoryName){
+            var deffered = $q.defer();
+
+            function findCategory() {
+                return _.find(categories, function(c) {
+                    return c.name == categoryName;
+                })
+            }
+
+            if(categories) {
+                deffered.resolve(findCategory());
+            } else {
+                model.getCategories()
+                    .then(function (result) {
+                        deffered.resolve(findCategory())
+                    })
+            }
+            return deffered.promise;
         }
     })
 ;
