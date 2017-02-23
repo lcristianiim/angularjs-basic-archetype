08b4606ba8af071716b7fe46895317a8b717061c
Cristian <lcristianiim@yahoo.com>
2017-02-23 17:23:09 +0200

    Items create js and tmpl

--- categories/items/create/item-create.js
+++ categories/items/create/item-create.js
@@ -1,1 +1,50 @@
-angular.module("categories.items.create", []);
+angular.module("categories.items.create", [
+
+])
+    .config(function($stateProvider){
+        $stateProvider
+            .state("ticketer.categories.items.create", {
+                url: "/items/create",
+                templateUrl: "categories/items/create/item-create.tmpl.html",
+                controller: "CreateItemsController as createItemsController"
+            })
+    })
+    .controller("CreateItemsController", function($state, $stateParams, CategoriesModel, ItemsModel) {
+        var createItemsController = this;
+
+        CategoriesModel.getCategories()
+            .then(function (categories) {
+                createItemsController.categories = categories;
+            })
+        ;
+
+        function returnToItems() {
+            $state.go("ticketer.categories.items", {
+                category: $stateParams.category
+            });
+        }
+
+        function cancelCreating() {
+            console.log("Arrived at cancel creating");
+            returnToItems();
+        }
+
+        function createItem(item) {
+            ItemsModel.createItem(item);
+            returnToItems();
+        }
+
+        function resetForm() {
+           createItemsController.newItem = {
+               title: "",
+               url: "",
+               category: $stateParams.category
+           }
+        }
+
+        resetForm();
+
+        createItemsController.cancelCreating = cancelCreating;
+        createItemsController.createItem = createItem;
+    })
+;
